1414415b070b803b35f37aadde3173c7ef646180
SKYoooon <skyoon7899@gmail.com>
2025-03-16 19:42:25 +0900

    0316

--- Baekjoon/10828.py
+++ Baekjoon/10828.py
@@ -1,3 +1,4 @@
+#1
 import sys
 stack = []
 num_of_input = int(sys.stdin.readline())
@@ -24,3 +25,25 @@ for _ in range(num_of_input):
             print(stack.pop())
         else:
             print(-1)
+
+
+# 2
+import sys
+
+stack = []
+num_of_input = int(sys.stdin.readline().strip())
+
+for _ in range(num_of_input):
+    input_data = sys.stdin.readline().strip().split()
+    command = input_data[0]
+
+    if command == 'push':
+        stack.append(int(input_data[1]))
+    elif command == 'top':
+        output.append(str(stack[-1] if stack else -1))
+    elif command == 'size':
+        output.append(str(len(stack)))
+    elif command == 'empty':
+        output.append(str(0 if stack else 1))
+    elif command == 'pop':
+        output.append(str(stack.pop() if stack else -1))
